3a28bebe426340be67aa14503744a54e5f86a370
beyoubala <balajiv.b25@gmail.com>
2025-09-20 13:42:25 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,10 @@
 output "ec2_public_ip" {
   value = aws_instance.ubuntu.public_ip
 }
-
+output "ec2_id" {
+  value = aws_instance.my_instance.id
+}
 output "web_url" {
   value = "http://${aws_instance.ubuntu.public_ip}"
+
 }
