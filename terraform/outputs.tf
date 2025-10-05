213f3a89ceccc1bd00642448bd231a8c9feffcef
beyoubala <balajiv.b25@gmail.com>
2025-10-05 20:40:03 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,6 @@
-output "image_url" {
-  value = "https://${aws_s3_bucket.public_bucket.bucket}.s3.amazonaws.com/${aws_s3_object.image.key}"
+output "gif_url" {
+  value = "https://${aws_s3_bucket.public_bucket.bucket}.s3.amazonaws.com/${aws_s3_object.gif_image.key}"
 }
+
 
 
